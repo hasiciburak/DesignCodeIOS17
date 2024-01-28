3c58410304930dccb1a89a5573eba4b32e706f15
Burak <burakhasici@gmail.com>
2024-01-28 12:29:45 +0300

    Added Files and First Card Component

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,17 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .center){
+            Image(systemName: "sparkles")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Hello")
+            Text("modern architecture, an isometric tiny house")
+                .font(.subheadline)
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Size")
+                        .foregroundColor(Color.secondary)
+                    Text("1024x1024")
+                        .fontWeight(.semibold)
+                }
+                Divider()
+                VStack(alignment: .leading){
+                    Text("Type")
+                        .foregroundColor(Color.secondary)
+                    Text("Upscale")
+                        .fontWeight(.semibold)
+                }
+                Divider()
+                    .frame(width: 2)
+                VStack(alignment: .leading){
+                    Text("Date")
+                        .foregroundColor(Color.secondary)
+                    Text("Today 5:19")
+                        .fontWeight(.semibold)
+                }
+            }
+            .frame(height: 44.0)
         }
-        .padding()
+        .padding(20.0)
+        .background(.green.gradient)
+        .cornerRadius(20)
     }
 }
-
 #Preview {
     ContentView()
 }
