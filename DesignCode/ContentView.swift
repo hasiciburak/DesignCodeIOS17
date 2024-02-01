a6dada2cd0e38e0fc0769ebd380c8cac419136c3
Burak <burakhasici@gmail.com>
2024-02-01 09:50:57 +0300

    Updated animations

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -1,124 +1,140 @@
-//
-//  ContentView.swift
-//  DesignCode
-//
-//  Created by Burak Haşıcı on 27.01.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State var isTapped = false
-
+    @State var time = Date.now
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var isActive = false
+    
     var body: some View {
         ZStack {
             Image(.image1)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(height: 300)
-                .cornerRadius(20)
+                .frame(height: isTapped ? 600 : 300)
+                .frame(width: isTapped ? 393 : 360)
+                .cornerRadius(isTapped ? 0 : 20)
+                .offset(y: isTapped ? -200 : 0)
+            
             VStack(alignment: .center) {
-                Text("modern architecture, an isometric tiny house")
+                Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
                     .font(.subheadline)
-                HStack {
+                HStack(spacing: 8.0) {
                     VStack(alignment: .leading) {
                         Text("Size")
-                            .foregroundColor(Color.secondary)
+                            .foregroundColor(.secondary)
                         Text("1024x1024")
-                            .fontWeight(.semibold)
                     }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
                     Divider()
+
                     VStack(alignment: .leading) {
                         Text("Type")
-                            .foregroundColor(Color.secondary)
+                            .foregroundColor(.secondary)
                         Text("Upscale")
-                            .fontWeight(.semibold)
                     }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
                     Divider()
-                        .frame(width: 2)
+                    
                     VStack(alignment: .leading) {
                         Text("Date")
-                            .foregroundColor(Color.secondary)
+                            .foregroundColor(.secondary)
                         Text("Today 5:19")
-                            .fontWeight(.semibold)
                     }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
                 }
                 .frame(height: 44.0)
-                // Options
+                
+                
                 HStack {
                     HStack {
                         Image(systemName: "ellipsis")
                             .symbolEffect(.pulse)
+                        Divider()
                         Image(systemName: "sparkle.magnifyingglass")
-                            .symbolEffect(.scale.up)
+                            .symbolEffect(.scale.up, isActive: isActive)
+                        Divider()
                         Image(systemName: "face.smiling")
+                            .symbolEffect(.appear, isActive: isActive)
                     }
                     .padding()
                     .frame(height: 44)
-                    .overlay(UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(
-                        topLeading: 0,
-                        bottomLeading: 20,
-                        bottomTrailing: 0,
-                        topTrailing: 20
-                    ))
-                    .strokeBorder(linearGradient)
+                    .overlay(
+                        UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 0, bottomLeading: 20, bottomTrailing: 0, topTrailing: 20))
+                            .strokeBorder(linearGradient)
                     )
                     .offset(x: -20, y: 20)
+                    
                     Spacer()
+                    
                     Image(systemName: "square.and.arrow.down")
                         .padding()
                         .frame(height: 44)
-                        .overlay(UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(
-                            topLeading: 20,
-                            bottomLeading: 0,
-                            bottomTrailing: 20,
-                            topTrailing: 0
-                        ))
-                        .strokeBorder(linearGradient)
+                        .overlay(
+                            UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 20, bottomLeading: 0, bottomTrailing: 20, topTrailing: 0))
+                                .strokeBorder(linearGradient)
                         )
                         .offset(x: 20, y: 20)
                 }
             }
             .padding(20.0)
             .background(.regularMaterial)
-            .overlay(RoundedRectangle(cornerRadius: 20)
-                .strokeBorder(linearGradient)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(linearGradient)
             )
-            .cornerRadius(20)
-            .padding(20)
-            .offset(y: 80)
-
+            .cornerRadius(20.0)
+            .padding(40)
+            .offset(y: isTapped ? 220 : 80)
+            
             HStack(spacing: 30) {
                 Image(systemName: "wand.and.rays")
-                    .symbolEffect(.variableColor.iterative.reversing)
+                    .frame(width: 44)
+                    .symbolEffect(.variableColor.iterative.reversing, options: .speed(3))
+                    .symbolEffect(.bounce, value: isTapped)
+                    .opacity(isTapped ? 1 : 0)
+                    .blur(radius: isTapped ? 0 : 20)
                 Image(systemName: isTapped ? "pause.fill" : "play.fill")
                     .frame(width: 44)
                     .contentTransition(.symbolEffect(.replace))
                     .onTapGesture {
-                        isTapped.toggle()
+                        withAnimation(.bouncy) {
+                            isTapped.toggle()
+                        }
                     }
                 Image(systemName: "bell.and.waves.left.and.right.fill")
-                    .symbolEffect(.bounce, options: .speed(3).repeat(3), value: 1)
+                    .frame(width: 44)
+                    .symbolEffect(.bounce, options: .speed(3).repeat(3), value: isTapped)
+                    .opacity(isTapped ? 1 : 0)
+                    .blur(radius: isTapped ? 0 : 20)
+                    .onReceive(timer) { value in
+                        time = value
+                        isActive.toggle()
+                    }
             }
+            .frame(width: isTapped ? 220 : 50)
+            .foregroundStyle(.primary, .white)
             .font(.largeTitle)
             .padding(20)
             .background(.ultraThinMaterial)
-            .overlay(RoundedRectangle(cornerRadius: 20)
-                .strokeBorder(linearGradient))
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(linearGradient)
+            )
             .cornerRadius(20)
+            .offset(y: isTapped ? 40 : -44)
         }
         .frame(maxWidth: 400)
-        .padding(20)
-        // Provides with elements from size to to size...
         .dynamicTypeSize(.xSmall ... .xLarge)
     }
-
+    
     var linearGradient: LinearGradient {
-        LinearGradient(
-            colors: [.clear, .primary.opacity(0.3), .clear],
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
+        LinearGradient(colors: [.clear, .primary.opacity(0.3), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)
     }
 }
 
